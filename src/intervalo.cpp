59d68a3c5b79b50a4269fe86af3023a790547781
thehaias <45339269+thehaias@users.noreply.github.com>
2020-02-19 18:30:54 +0100

    Add files via upload

---
+++ src/intervalo.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "intervalo.h"
+using namespace std;
+
+
+bool Intervalo::valido(double cinf,double csup, bool cerrinf, bool cerrsup){
+   return (( cinf < csup ) || ( cinf == csup && cerrinf == cerrsup));
+}
+
+void escribir(const Intervalo & obj) {
+    if (obj.esVacio())
+        cout << "(0)";
+    else {
+        if (obj.dentroCotaInf())
+         cout << '[';
+        else cout << '(';
+        cout << obj.getCotaInf() << "," << obj.getCotaSup();
+        if (obj.dentroCotaSup())
+            cout << ']';
+        else cout << ')';
+    }
+}
+
+void leer(Intervalo & obj){
+    // Formato de lectura del intervalo: [|( x,y )|]
+    bool cerradoInf = true;
+    bool cerradoSup = true;
+    double cotaInf, cotaSup;
+    char car;
+    cin >> car;
+    if (car == '(')
+        cerradoInf = false;
+    cin >> cotaInf;
+    cin >> car;
+    cin >> cotaSup;
+    cin >> car;
+    if (car == ')')
+        cerradoSup = false;
+    obj.setIntervalo(cotaInf, cotaSup, cerradoInf, cerradoSup);
+}
+
+void comprobarVacio(Intervalo obj){
+	 escribir(obj);
+    if (obj.esVacio())
+        cout << " Vacio";
+    else  cout << " NO Vacio";
+    cout << endl;
+}
